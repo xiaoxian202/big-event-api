c7d99a1dd7d000b7b341ad0cdf76002c70210727
xiaodu <you@example.com>
2020-07-01 14:07:42 +0800

    登录

--- routers/login.js
+++ routers/login.js
@@ -5,11 +5,39 @@ const express = require('express')
 const path = require('path')
 //导入第三方包 加密 先下载依赖包
 const utils = require('utility')
+//token生成的规范 安装依赖包
+const jwt = require('jsonwebtoken')
 const db = require(path.join(__dirname,'../common/db.js'))
 const router = express.Router()
 
-router.post('/login',(req,res) => {
-    res.end('login')
+//登录
+router.post('/login',async (req,res) => {
+    // 获取客户端提交的参数
+    let param = req.body
+    //注意:密码必须先进行加密
+    param.password = utils.md5(req.body.password)
+    //根据用户名查询数据库
+    let sql = 'select id from user where username = ? and password =?'
+    let ret = await db.operateData(sql,[param.username,param.password])
+    // 如果是查询，那么ret是数组，如果是增删改，那么ret是对象
+    if(ret && ret.length > 0){
+        //如果登录验证通过
+        let token = jwt.sign(
+            {username:req.body.username,id:ret[0].id},
+            'bigevent',
+            {expiresIn:'1h'}
+        )
+        res.json({
+            status:0,
+            message:'登录成功',
+            token:'Bearer ' + token
+        })
+    }else {
+        res.json({
+            status:1,
+            message:'登录失败'
+        })
+    }
 })
 
 // 注册用户接口
